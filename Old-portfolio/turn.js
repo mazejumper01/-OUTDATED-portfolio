2ef242dae9f35208459f99a4399366e23b53e264
mazejumper01 <94059641+mazejumper01@users.noreply.github.com>
2024-04-24 17:26:03 +0200

    Add files via upload

---
+++ Old-portfolio/turn.js
@@ -0,0 +1,15 @@
+const range = document.querySelector("#range")
+const turnImg = document.querySelector("#turn-img")
+
+range.addEventListener("input", function(){
+    console.log(this.value)
+    let extraDigit = "0"
+    if (this.value > 9){
+    extraDigit = ""
+    }
+    else{
+        extraDigit = "0"
+    }
+    turnImg.setAttribute("src", "turntable-frames/try00" + extraDigit + this.value + ".png")
+    
+})
